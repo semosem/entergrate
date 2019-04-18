9b39171b3e7375ba899b6d71d99b26c5b72cddf7
sem.gebresilassie@gmail.com <sem.gebresilassie@gmail.com>
2019-04-18 22:32:23 +0300

    clean up and make deployable

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
-const transporter = require('./utils/transporter');
+// const transporter = require('./utils/transporter');
 
 app.use("/js", express.static(__dirname + "/js"));
 app.use("/css", express.static(__dirname + "/css"));
@@ -12,7 +12,7 @@ app.use("/images", express.static(__dirname + "/images"));
 app.use(bodyParser.urlencoded({
     extended: false
  }));
- 
+
 app.use(bodyParser.json());
 
 // viewed at http://localhost:8080
@@ -20,34 +20,34 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.post('/contact',function(req,res){
-    const { email, fullName, message } = req.body;
-    transporter.sendMail(
-        {
-            from : 'info@entergrate.org',
-            to : 'info@entergrate.org',
-            subject : `Message from ${fullName}`,
-            replyTo: email,
-            html: `<div>
-                <h3>
-                    <p>From : ${fullName}</p>
-                    <p>Email: ${email}</p>
-                </h3>
-                <div style="margin: 30px; font-family: 'Overpass', Arial, sans-serif; font-size: 16px; line-height: 1.8; color: gray; font-weight: 300;">${message}</div>
-            </div>`
-        },
-        (err, info) => {
-            if (err) {
-                console.log(err);
-                return err
-            } else {
-                console.log({
-                    success: `Message sent: ${info.response}`
-                });
-                return {success: `Message sent: ${info.response}`, res : res}
-            }
-        }
-    );
-});
+// app.post('/contact',function(req,res){
+//     const { email, fullName, message } = req.body;
+//     transporter.sendMail(
+//         {
+//             from : 'info@entergrate.org',
+//             to : 'info@entergrate.org',
+//             subject : `Message from ${fullName}`,
+//             replyTo: email,
+//             html: `<div>
+//                 <h3>
+//                     <p>From : ${fullName}</p>
+//                     <p>Email: ${email}</p>
+//                 </h3>
+//                 <div style="margin: 30px; font-family: 'Overpass', Arial, sans-serif; font-size: 16px; line-height: 1.8; color: gray; font-weight: 300;">${message}</div>
+//             </div>`
+//         },
+//         (err, info) => {
+//             if (err) {
+//                 console.log(err);
+//                 return err
+//             } else {
+//                 console.log({
+//                     success: `Message sent: ${info.response}`
+//                 });
+//                 return {success: `Message sent: ${info.response}`, res : res}
+//             }
+//         }
+//     );
+// });
 
 app.listen(8080);
